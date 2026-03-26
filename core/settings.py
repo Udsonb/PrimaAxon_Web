d1064f26a8d0af4f99dd12da73f75822d47ebaf6
Udson <udsonb@gmail.com>
2026-03-26 10:17:13 -0300

    resolvendo logo

--- core/settings.py
+++ core/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # <-- FALTAVA ESTA LINHA AQUI!
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
